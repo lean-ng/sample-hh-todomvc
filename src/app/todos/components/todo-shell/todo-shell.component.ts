40ef9b898021ed068972a041ac2a9cde6c34ea0c
Micha Alt <mail@lean-stack.de>
2019-10-30 19:06:55 +0100

    todos: handling create and displaying todos.

--- src/app/todos/components/todo-shell/todo-shell.component.ts
+++ src/app/todos/components/todo-shell/todo-shell.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'todo-shell',
   templateUrl: './todo-shell.component.html',
   styles: []
 })
-export class TodoShellComponent implements OnInit {
+export class TodoShellComponent {
 
-  constructor() { }
+  // Component State
+  todoList: Todo[] = [];
 
-  ngOnInit() {
+  // Template Event Handlers
+  createTodo(title: string) {
+    const id = this.todoList ? 1 : this.todoList[this.todoList.length - 1].id;
+    this.todoList.push({ id, title, completed: false });
   }
-
 }
--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TodoShellComponent } from './components/todo-shell/todo-shell.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     TodoShellComponent,
-    TodoInputComponent
+    TodoInputComponent,
+    TodoItemComponent
   ],
   imports: [
     CommonModule
