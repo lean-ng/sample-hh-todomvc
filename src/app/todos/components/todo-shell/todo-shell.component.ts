9d194ecb676b511c7098dd35633a4427c9cb3172
Micha Alt <mail@lean-stack.de>
2019-10-30 20:34:00 +0100

    todos: handle remove completed request

--- src/app/todos/components/todo-shell/todo-shell.component.ts
+++ src/app/todos/components/todo-shell/todo-shell.component.ts
@@ -32,6 +32,10 @@ export class TodoShellComponent {
     const ix = this.todoList.indexOf(todo);
     this.todoList.splice(ix, 1);
   }
+  removeCompleted() {
+    this.todoList = this.todoList.filter( t => !t.completed);
+  }
+
   setAllCompletedStates(completed: boolean) {
     this.todoList.forEach(t => t.completed = completed);
   }
