6a1ab99852c598068a573c60b4424149db3b647d
Micha Alt <mail@lean-stack.de>
2019-10-30 19:11:19 +0100

    Rendering an input todo.

--- src/app/todos/components/todo-item/todo-item.component.ts
+++ src/app/todos/components/todo-item/todo-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'todo-item',
   templateUrl: './todo-item.component.html',
   styles: []
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  // Public Input Properties
+  @Input()
+  todo: Todo;
 
 }
