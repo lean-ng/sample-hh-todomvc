b3d97ddc3b03dfc3586580e9ac1debf427fccb76
Micha Alt <mail@lean-stack.de>
2019-10-30 18:45:13 +0100

    TodoMVC template imported and a static usage.

---
+++ src/app/todos/components/todo-shell/todo-shell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-shell',
+  templateUrl: './todo-shell.component.html',
+  styles: []
+})
+export class TodoShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoShellComponent } from './components/todo-shell/todo-shell.component';
+
+@NgModule({
+  declarations: [
+    TodoShellComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TodoShellComponent
+  ]
+})
+export class TodosModule { }
