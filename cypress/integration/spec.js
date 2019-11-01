2bbde7cdcbd93d348524f36126b2616240737d23
Micha Alt <mail@lean-stack.de>
2019-11-01 15:49:45 +0100

    Item handling.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -130,7 +130,7 @@ describe('Angular TodoMVC', () => {
     });
   });
 
-  context('Item', () => {
+  context.skip('Item', () => {
     it('should allow me to mark items as complete', () => {
       cy.createTodo(todoFixtures[0]).as('firstTodo');
       cy.createTodo(todoFixtures[1]).as('secondTodo');
@@ -168,13 +168,302 @@ describe('Angular TodoMVC', () => {
       cy.get('@todo').find('label').should('contain.text', 'E2E Testing with Cypress');
     });
 
+    it('should save edits on blur', function () {
+      cy.createTodo(todoFixtures[0]);
+      cy.createTodo(todoFixtures[1]).as('todo');
+
+      cy.get('@todo').find('label').dblclick();
+      cy.get('@todo').find('.edit').should('have.value', todoFixtures[1])
+        .clear()
+        .type('E2E Testing with Cypress')
+        .blur();
+      cy.get('@todo').find('label').should('contain.text', 'E2E Testing with Cypress');
+    });
+
     it('should allow me to delete an item', () => {
       cy.createTodo(todoFixtures[0]).as('todo');
       cy.createTodo(todoFixtures[1]);
 
-      cy.get('@todo').find('.destroy').click();
+      cy.get('@todo').find('.destroy').click({force: true});
       cy.get(selectors.todoItems).should('have.length', 1);
       cy.get(selectors.todoItems).first().contains(todoFixtures[1]);
     });
+
+    it('should remove the item if an empty text string was entered', () => {
+      cy.createTodo(todoFixtures[0]).as('todo');
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get('@todo').find('label').dblclick();
+      cy.get('@todo')
+        .find('.edit')
+        .clear().type('{enter}');
+
+      cy.get(selectors.todoItems).should('have.length', 1);
+      cy.get(selectors.todoItems).first().contains(todoFixtures[1]);
+    });
+
+    it.skip('should persist marking/un-marking of items as complete', () => {
+      cy.createTodo(todoFixtures[0]);
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get(selectors.todoItems).last().find('.toggle').check();
+      cy.reload();
+      cy.get(selectors.todoItems).last().should('have.class', 'completed');
+
+      cy.get(selectors.todoItems).last().find('.toggle').check();
+      cy.reload();
+      cy.get(selectors.todoItems).last().should('not.have.class', 'completed');
+    });
+
+    it.skip('should persist editing of items', () => {
+      cy.createTodo(todoFixtures[0]);
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get(selectors.todoItems).last().find('label').dblclick();
+      cy.get(selectors.todoItems).last().find('.edit').clear()
+        .type('E2E Testing with Cypress{enter}');
+      cy.reload();
+
+      cy.get(selectors.todoItems).last().find('label').should('contain.text', 'E2E Testing with Cypress');
+    });
+
+    it.skip('should persist edits commited via blur', function () {
+      cy.createTodo(todoFixtures[0]);
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get(selectors.todoItems).last().find('label').dblclick();
+      cy.get(selectors.todoItems).last().find('.edit')
+      .clear()
+        .type('E2E Testing with Cypress')
+        .blur();
+      cy.reload();
+
+      cy.get(selectors.todoItems).last().find('label').should('contain.text', 'E2E Testing with Cypress');
+    });
+
+    it.skip('should persist deleting items', () => {
+      cy.createTodo(todoFixtures[0]).as('todo');
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get('@todo').find('.destroy').invoke('show').click();
+      cy.reload();
+      cy.get(selectors.todoItems).should('have.length', 1);
+    });
+
+    it.skip('should persist deleting items via empty edit text', () => {
+      cy.createTodo(todoFixtures[0]).as('todo');
+      cy.createTodo(todoFixtures[1]);
+
+      cy.get('@todo').find('label').dblclick();
+      cy.get('@todo')
+        .find('.edit')
+        .clear().type('{enter}');
+      cy.reload();
+
+      cy.get(selectors.todoItems).should('have.length', 1);
+    });
   });
+
+  context('Editing', () => {
+    beforeEach(() => {
+      cy.createTodo(todoFixtures[0]);
+      cy.createTodo(todoFixtures[1]);
+      cy.createTodo(todoFixtures[2]);
+      cy.get(selectors.todoItems).as('todos');
+    });
+
+    it('should hide edit fields when not editing', () => {
+      cy.get('@todos').find('.edit').should('not.be.visible');
+    });
+
+    it('should hide other controls when editing', function () {
+      cy.get('@todos').eq(1).find('label').dblclick();
+
+      cy.get('@todos').eq(1).find('.toggle').should('not.be.visible');
+      cy.get('@todos').eq(1).find('label').should('not.be.visible');
+    });
+
+    it('should allow only one editing element', () => {
+      cy.get('@todos').eq(1).find('label').dblclick();
+      cy.get('@todos').eq(2).find('label').dblclick();
+
+      cy.get('@todos').eq(1).find('.edit').should('not.be.visible');
+      cy.get('@todos').eq(2).find('.edit').should('be.visible');
+    });
+
+    it('should focus on the edited todo after entering edit mode', () => {
+      cy.get('@todos').eq(1).find('label').dblclick();
+      cy.get('@todos').eq(1).find('.edit').should('have.focus');
+    });
+
+    it('should trim entered text', () => {
+      cy.get('@todos').eq(1).find('label').dblclick();
+
+      cy
+        .get('@todos')
+        .eq(1)
+        .find('.edit')
+        .type('{selectall}{backspace}    E2E Testing with Cypress    {enter}');
+
+        cy.get('@todos').eq(1).find('label').should('contain.text', 'E2E Testing with Cypress');
+    });
+
+    it('should cancel edits on escape', () => {
+      cy.get('@todos').eq(1).find('label').dblclick();
+
+      cy
+        .get('@todos')
+        .eq(1)
+        .find('.edit')
+        .type('{selectall}{backspace}Protractor Testing{esc}')
+
+      cy.get('@todos').eq(1).find('label').should('contain.text', todoFixtures[1]);
+    });
+  });
+
+  /*
+ context('Counter', function () {
+      it('should display the current number of todo items', function () {
+        cy.createTodo(TODO_ITEM_ONE)
+        cy.get(selectors.count).contains('1')
+        cy.createTodo(TODO_ITEM_TWO)
+        cy.get(selectors.count).contains('2')
+        checkNumberOfTodosInLocalStorage(2)
+      })
+    })
+
+    context('Clear completed button', function () {
+      beforeEach(function () {
+        cy.createDefaultTodos().as('todos')
+      })
+
+      it('should display the correct text', function () {
+        cy.get('@todos').eq(0).find('.toggle').check()
+        cy.get(selectors.clearCompleted).contains('Clear completed')
+      })
+
+      it('should remove completed items when clicked', function () {
+        cy.get('@todos').eq(1).find('.toggle').check()
+        cy.get(selectors.clearCompleted).click()
+        cy.get('@todos').should('have.length', 2)
+        cy.get('@todos').eq(0).should('contain', TODO_ITEM_ONE)
+        cy.get('@todos').eq(1).should('contain', TODO_ITEM_THREE)
+      })
+
+      it('should be hidden when there are no items that are completed', function () {
+        cy.get('@todos').eq(1).find('.toggle').check()
+        cy.get(selectors.clearCompleted).should('be.visible').click()
+        cy.get(selectors.clearCompleted).should('not.be.visible')
+      })
+    })
+
+    context('Persistence', function () {
+      it('should persist its data', function () {
+        // mimicking TodoMVC tests
+        // by writing out this function
+        function testState () {
+          cy
+            .get('@firstTodo')
+            .should('contain', TODO_ITEM_ONE)
+            .and('have.class', 'completed')
+          cy
+            .get('@secondTodo')
+            .should('contain', TODO_ITEM_TWO)
+            .and('not.have.class', 'completed')
+        }
+
+        cy.createTodo(TODO_ITEM_ONE).as('firstTodo')
+        cy.createTodo(TODO_ITEM_TWO).as('secondTodo')
+        cy.get('@firstTodo').find('.toggle').check().then(testState)
+        // at this point, the app might still not save
+        // the items in the local storage, for example KnockoutJS
+        // first recomputes the items and still have "[]"
+        checkTodosInLocalStorage(TODO_ITEM_ONE, true)
+        checkCompletedKeywordInLocalStorage()
+
+        // but there should be 1 completed item
+        checkNumberOfCompletedTodosInLocalStorage(1)
+
+        // now can reload
+        cy.reload().then(testState)
+      })
+    })
+
+    context('Routing', function () {
+      beforeEach(function () {
+        cy.createDefaultTodos().as('todos')
+        // make sure the app had a chance to save updated todos in storage
+        // before navigating to a new view, otherwise the items can get lost :(
+        // in some frameworks like Durandal
+        checkTodosInLocalStorage(TODO_ITEM_ONE)
+      })
+
+      it('should allow me to display active items', function () {
+        cy.get('@todos').eq(1).find('.toggle').check()
+        checkNumberOfCompletedTodosInLocalStorage(1)
+        cy.contains(selectors.filterItems, 'Active').click()
+        visibleTodos()
+          .should('have.length', 2)
+          .eq(0)
+          .should('contain', TODO_ITEM_ONE)
+        visibleTodos().eq(1).should('contain', TODO_ITEM_THREE)
+      })
+
+      it('should respect the back button', function () {
+        cy.get('@todos').eq(1).find('.toggle').check()
+        checkNumberOfCompletedTodosInLocalStorage(1)
+
+        cy.log('Showing all items')
+        cy.contains(selectors.filterItems, 'All').click()
+        visibleTodos().should('have.length', 3)
+
+        cy.log('Showing active items')
+        cy.contains(selectors.filterItems, 'Active').click()
+        cy.log('Showing completed items')
+        cy.contains(selectors.filterItems, 'Completed').click()
+        visibleTodos().should('have.length', 1)
+
+        cy.log('Back to active items')
+        cy.go('back')
+        visibleTodos().should('have.length', 2)
+
+        cy.log('Back to all items')
+        cy.go('back')
+        visibleTodos().should('have.length', 3)
+      })
+
+      it('should allow me to display completed items', function () {
+        visibleTodos().eq(1).find('.toggle').check()
+        checkNumberOfCompletedTodosInLocalStorage(1)
+        cy.get(selectors.filters).contains('Completed').click()
+        visibleTodos().should('have.length', 1)
+      })
+
+      it('should allow me to display all items', function () {
+        visibleTodos().eq(1).find('.toggle').check()
+        checkNumberOfCompletedTodosInLocalStorage(1)
+        cy.get(selectors.filters).contains('Active').click()
+        cy.get(selectors.filters).contains('Completed').click()
+        cy.get(selectors.filters).contains('All').click()
+        visibleTodos().should('have.length', 3)
+      })
+
+      it('should highlight the currently applied filter', function () {
+        cy
+          .contains(selectors.filterItems, 'All')
+          .should('have.class', 'selected')
+        cy.contains(selectors.filterItems, 'Active').click()
+        // page change - active items
+        cy
+          .contains(selectors.filterItems, 'Active')
+          .should('have.class', 'selected')
+        cy.contains(selectors.filterItems, 'Completed').click()
+        // page change - completed items
+        cy
+          .contains(selectors.filterItems, 'Completed')
+          .should('have.class', 'selected')
+      })
+    })
+  })
+  */
 });
